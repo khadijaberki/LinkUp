445e3d5366f78c63c35c4a2bd23cc4e2ed83c186
khadijaberki <khadija.berki24@ump.ac.ma>
2026-01-17 16:50:56 +0100

    profiiiil

--- authoo/models.py
+++ authoo/models.py
@@ -62,10 +62,17 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+from django.db import models
+from django.contrib.auth.models import User
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} : {self.content}"
+
 class Friend(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_friends")
     friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_of")
